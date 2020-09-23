eb8d9aa63f67d12e6a9933221d64340c1b46d846
Christorres392 <christorres09@gmail.com>
2020-09-23 11:03:58 -0600

    Submission for deadline

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,10 +1,47 @@
 import React, { Component } from "react";
+import BotCollection from './BotCollection';
+import YourBotArmy from './YourBotArmy';
 
 class BotsPage extends Component {
   //start here with your code for step one
 
+  state = {
+    bots: []
+    
+  }
+
+  componentDidMount() {
+    this.fetchBots()
+  }
+
+  fetchBots() {
+    fetch("http://localhost:6001/bots")
+      .then(res => res.json())
+      .then(bots => this.setState({ bots: bots }))
+  }
+
+  joinArmy = (bots) => {
+    let botArmy = this.state.bots.map(bot => {
+      let newBot = { ...bot }
+      if (bots.id === bot.id) {
+        newBot.army = true
+      }
+        return newBot
+    })
+    this.setState({ bots: botArmy })
+  }
+
+  removeBot = (event) => {
+    console.log("removing Bot")
+    event.preventDefault()
+  }
+
+
   render() {
-    return <div>{/* put your components here */}</div>;
+    return <div>
+      <BotCollection joinArmy={this.joinArmy} bots={this.state.bots} />
+      <YourBotArmy removeBot={this.removeBot} botArmy={this.state.bots.filter(bot => bot.army)}/>
+    </div>;
   }
 }
 
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
+import BotCard from "../components/BotCard";
 
 class YourBotArmy extends Component {
   //your bot army code here...
 
-  render() {
+  render() { console.log(this.props)
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
+            {this.props.botArmy.map(bot => <BotCard handleDeleteClick={this.props.removeBot} key={bot.id} bot={bot}/>)}
             Your Bot Army
           </div>
         </div>
