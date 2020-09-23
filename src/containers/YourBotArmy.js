3e255a19acd6d91a00b6551cdf13ed9dffff7385
Ceddyp19 <ceddyp117@gmail.com>
2020-09-23 12:07:55 -0500

    submission for deadline

--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import BotCard from '/Users/cedricpatton/Flatiron/code/Mod4/Code_Challenge/module-4-code-challenge-round-1-atx01-seng-ft-071320-bot-battlr-1600873188/src/components/BotCard.js'
 
 class YourBotArmy extends Component {
   //your bot army code here...
@@ -8,8 +9,10 @@ class YourBotArmy extends Component {
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
-            Your Bot Army
+          Your Bot Army<br/>
+            {this.props.army.map(bot => (
+              <BotCard key={bot.id} bot={bot} handleClick={this.props.releaseBot} handleButton={this.props.deleteBot} />
+            ))}
           </div>
         </div>
       </div>
