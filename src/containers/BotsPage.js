9e1b32468b958cb31ce421f3120b0f373a78b302
timrinkerman <tim.rinkerman98@gmail.com>
2020-09-23 12:10:42 -0500

    submission for deadline

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,10 +1,59 @@
 import React, { Component } from "react";
-
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
+//that dang time limit
 class BotsPage extends Component {
-  //start here with your code for step one
+  state = {
+    robots: [],
+    myArmy: []
+  }
+  
+  
+  componentDidMount(){
+    fetch('http://localhost:6001/bots')
+    .then(res => res.json())
+    .then(data => this.setState({robots: data}))
+  }
+  
+
+  dischargeRobot = (robot) => {
+   if (this.state.myArmy.includes(robot) === true){
+    const array = this.state.myArmy;
+    const index = array.indexOf(robot); 
+     array.splice(index)
+   }
+  }
+
+
+  addToMyArmy = (robot) =>{
+    // const enlistedRobot = this.state.robots.map(soldier => {
+    //   const newRobot = {...soldier}
+    //   if (robot.id === soldier.id){
+    //     newRobot.enlisted === true
+    //   }
+    //   return newRobot
+    // });
+    // this.setState({myArmy:
+
+    if (this.state.myArmy.includes(robot) === true){
+      null
+    }else{
+    this.setState({myArmy: [...this.state.myArmy,robot]})
+  }}
+
+
+
 
   render() {
-    return <div>{/* put your components here */}</div>;
+    return <div>
+       <YourBotArmy myArmy={this.state.myArmy}
+       dischargeRobot={this.dischargeRobot}/>
+      <BotCollection 
+      robots={this.state.robots}
+      addToMyArmy={this.addToMyArmy}/>
+     
+      {/* put your components here */}
+      </div>;
   }
 }
 
